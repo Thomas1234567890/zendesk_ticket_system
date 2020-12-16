cd6bae4d80d5ef16c69bf77d96808349fc694e27
Thomas1234567890 <tompatcon@gmail.com>
2020-12-16 16:04:16 +0000

    3 files modified, 4 added. Boilerplate with jQuery dependencies added

---
+++ app/script.ts
@@ -0,0 +1,13 @@
+// Zendesk Ticket Viewer
+
+///define
+
+// 	Connect to the Zendesk API / load local ticket array instead
+// 	Request all the tickets for your account / let the user pick tickets tied to a user acount
+// 	Display them in a list
+// 	Display individual ticket details
+// 	Page through tickets when more than 25 are returned / pagnation
+
+///PRODUCTION
+
+/// NOTE : the data.json is contained in a ticket property.
---
+++ app/test_script.ts
@@ -0,0 +1,13 @@
+
+
+
+let data_address = './test_file/data.json';
+
+let return_json_array = (data_address) => {
+  // let json = $.getJSON({ url: `${data_address}`, async: false });
+  // json = JSON.parse(json.responseText);
+  // return json;
+  
+  
+
+};
